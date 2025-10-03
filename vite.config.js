9ba06b60d8e98722a7e8e643f8bb08f369e280c1
Linu12 <tomecki1212@gmail.com>
2025-10-03 19:32:06 +0200

    Fix rollup optional dependency issue

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite'
+//
+// export default defineConfig({
+// 	plugins: [
+// 		sveltekit(),
+// 		tailwindcss()
+// 	],
+// 	server: {
+// 		host: '0.0.0.0', // Umożliwi dostęp z innych urządzeń
+// 		port: 5173      // (opcjonalnie) port do użycia
+// 	}
+//
+// });
+
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
-		tailwindcss()
+		sveltekit()
 	],
 	server: {
-		host: '0.0.0.0', // Umożliwi dostęp z innych urządzeń
-		port: 5173      // (opcjonalnie) port do użycia
+		host: '0.0.0.0',
+		port: 5173
 	}
-
 });
